6f869c3a303193ee32b22868e5ee88ab357d5744
lukadulac <lukadulac4@gmail.com>
2026-01-17 15:30:26 +0100

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
 		default: "HarmonijaVita - Stručna podrška za rast i razvoj",
 		template: "%s | HarmonijaVita",
 	},
+
 	description:
 		"Stručna podrška za rast i razvoj, za promenu, zdravije odnose i ispunjen život. Profesionalno psihološko savetovanje i terapija.",
 	keywords: [
@@ -72,11 +73,23 @@ export const metadata: Metadata = {
 	},
 	icons: {
 		icon: [
-			{ url: "/favicon-16x16.ico", sizes: "48x48", type: "image/x-icon" },
-			{ url: "/favicon-32x32.png", sizes: "192x192", type: "image/png" },
+			{ url: "/favicon.ico", sizes: "any" },
+			{ url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+			{ url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+			{
+				url: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				url: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
 		],
-		shortcut: "/favicon.ico",
-		apple: { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+		apple: [
+			{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+		],
 	},
 };
 
